1beb22a535eaca5c822abe3cd414b3f8d2563284
Walid <walid.elhmamsi@gmail.com>
2024-09-09 15:55:40 +0200

    commi

--- obj/Debug/net8.0/mywebapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/mywebapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mywebapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f26e3966c3312fd71a9ec874d12a5f52567f0a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a9e585c1f5d8764b657448ff6b2182488157b22")]
 [assembly: System.Reflection.AssemblyProductAttribute("mywebapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mywebapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
